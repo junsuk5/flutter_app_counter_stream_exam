4870d8253c25e41014b1499f24409168486876fa
Junsuk Oh <a811219@gmail.com>
2021-04-13 15:33:57 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/survivalcoding/flutter_app_stream/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.survivalcoding.flutter_app_stream
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/counter.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+class Counter {
+  int _counter = 0;
+
+  final _counterStreamController = StreamController<int>();
+
+  Stream<int> get counterStream => _counterStreamController.stream;
+
+  void increment() {
+    _counter++;
+    _counterStreamController.add(_counter);
+  }
+}
